f852bec89f6d0700649f2d010a912f0ca0de6156
notsilverman <birchba@gmail.com>
2018-09-16 22:50:40 -0500

    temp fix for placement and score goes up

--- src/App.js
+++ src/App.js
@@ -7,29 +7,45 @@ import bachelors from "./bachelors.json"
 
 class App extends Component {
   state = {
-    bachelors
+    bachelors,
+    score: 0,
+    topScore: 0
   };
 
   removeBachelor = id => {
     const bachelors = this.state.bachelors.filter(bachelor => bachelor.id !== id);
+    const score = this.state.score+1;
+    let topScore = this.state.topScore;
+    // if(this.state.topScore<score){
+    //   return topScore+1
+    // } else {
+    //   return topScore
+    // }
 
-    this.setState({ bachelors })
+    this.setState({ 
+      bachelors,
+      score,
+      topScore})
   }
+
+
   render() {
     return (
-      <Wrapper>
-        <Header>Let's Do The Damn Thing</Header>
-        {this.state.bachelors.map(bachelor => (
-          <Bachelor
-            removeBachelor={this.removeBachelor}
-            id={bachelor.id}
-            key={bachelor.id}
-            name={bachelor.name}
-            image={bachelor.image}
-            season={bachelor.season}
-          />
-        ))}
-      </Wrapper>
+      <div>
+        <Header score={this.state.score} topScore={this.state.topScore}>Let's Do The Damn Thing</Header>
+        <Wrapper>
+          {this.state.bachelors.map(bachelor => (
+            <Bachelor
+              removeBachelor={this.removeBachelor}
+              id={bachelor.id}
+              key={bachelor.id}
+              name={bachelor.name}
+              image={bachelor.image}
+              season={bachelor.season}
+            />
+          ))}
+        </Wrapper>
+      </div>
     );
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,10 @@ const Header = props => (
     <a className="navbar-brand" href="/">{props.children}</a>
     <div className="collapse navbar-collapse">
       <div className="text score" id="wins">
-        Score: 0
+        Score: {props.score}
       </div>
       <div className="text score" id="loss">
-        Top Score: 0
+        Top Score: {props.topScore}
       </div>
     </div>
   </nav>
