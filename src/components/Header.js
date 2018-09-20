bbb41b46db898eed7b9eba7ab08bee56c318b470
notsilverman <birchba@gmail.com>
2018-09-20 16:04:56 -0500

    fixed header for mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import "../styles/Header.css";
 const Header = props => (
   <nav className="navbar navbar-expand-lg">
     <a className="navbar-brand" href="/">{props.children}</a>
-    <div className="collapse navbar-collapse">
+    <div className="middleBit">
       <div className="text score" id="wins">
         Score: {props.score}
       </div>
